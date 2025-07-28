38a8c1adc4d1bab0cac4a41d84494c90723a28ef
Maxim Kirby <eosinyx34@gmail.com>
2025-07-28 10:48:51 -0400

    Implemented lr_scheduler to AstraModule

--- src/astra/model/models.py
+++ src/astra/model/models.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable
+from typing import Optional, Type
 
 import lightning as L
 import numpy as np
@@ -10,33 +11,66 @@ from sklearn.metrics import mean_squared_error
 
 
 class AstraModule(L.LightningModule):
-    def __init__(self, model: nn.Module, optimizer: torch.optim = None, lr: float = 1e-3, loss_func = None, recomposition_func: Callable = None):
+    def __init__(self, 
+                 model: nn.Module, 
+                 loss_func: nn.Module = None, 
+                 recomposition_func: Optional[Callable] = None, 
+                 optimizer_class: Type[torch.optim.Optimizer] = torch.optim.AdamW, 
+                 lr_scheduler_class: Optional[Type[torch.optim.lr_scheduler._LRlr_scheduler]] = None, 
+                 lr_scheduler_kwargs: Optional[dict] = None
+            ):
         """
         A flexible LightningModule that can optionally apply a final
         transformation function to the base model's output.
 
         Args:
-            model (nn.Module): The core model. It can either output the final
-                                    parameters or intermediate rates.
-            optimizer (torch.optim): A PyTorch optimizer used to train the model.
-            lr (float): The learning rate for the optimizer.
-            loss_func: A PyTorch loss function used to compute model performance 
-                       and update gradients during training.
-            recomposition_func (callable, optional): A function to transform
-                        the model's output. If None, the output
-                        is returned directly. Defaults to None.
+            model (nn.Module): The core model architecture.
+            lr (float): The learning rate.
+            recomposition_func (Optional[Callable]): A function to transform model output.
+            optimizer_class (Type[torch.optim.Optimizer]): The class of the optimizer to use.
+                Example: torch.optim.Adam, torch.optim.SGD.
+            loss_func (nn.Module): An instance of the loss function to use.
+                Defaults to a MaskedMSELoss that handles NaNs.
+            lr_scheduler_class (Optional[Type]): The class of the LR lr_scheduler to use.
+                Example: torch.optim.lr_scheduler.ReduceLROnPlateau.
+            lr_scheduler_kwargs (Optional[dict]): A dictionary of arguments for the lr_scheduler.
+                Must include a 'monitor' key for lr_schedulers that require one.
         """
         super().__init__()
+        self.save_hyperparameters(ignore=['model', 'recomposition_func', 'loss_func'])
+
         self.model = model
-        self.optimizer = torch.optim.Adam
-        self.lr = lr
-        self.loss_func = F.mse_loss
+        self.loss_func = loss_func
         self.recomposition_func = recomposition_func
+
+        self.optimizer_class = optimizer_class
+        self.lr_scheduler_class = lr_scheduler_class
+        self.lr_scheduler_kwargs = lr_scheduler_kwargs or {}
     
     def configure_optimizers(self):
-        return self.optimizer(self.parameters(), lr=self.lr)
+        """Instantiate the optimizer and, optionally, the lr_scheduler."""
+        optimizer = self.optimizer_class(self.parameters(), lr=self.hparams.lr)
+        
+        # If there's no lr_scheduler, return optimizer
+        if self.lr_scheduler_class is None:
+            return optimizer
+        
+        # If there's a lr_scheduler, return dict w/ optimizer and lr_scheduler
+        else:
+            lr_scheduler = self.lr_scheduler_class(optimizer, **self.lr_scheduler_kwargs)
+            
+            return {
+                "optimizer": optimizer,
+                "lr_scheduler": {
+                    "lr_scheduler": lr_scheduler,
+                    "monitor": self.lr_scheduler_kwargs.get("monitor", "val_loss"),
+                    "interval": "epoch",
+                    "frequency": 1,
+                },
+            }
     
     def _shared_step(self, batch):
+        """A flexible shared step for training and validation."""
         # Can use this to reduce replicate code in training_step() and validation_step()
 
         # Get inputs from batch
@@ -75,13 +109,6 @@ class AstraModule(L.LightningModule):
 
         # Return loss for optimizer
         return loss
-
-    def on_training_epoch_end(self):
-        # Can be used to log accumulated metrics from training_step()
-        #epoch_metric = self.train_metric.compute()
-        #self.log("train/metric_epoch", epoch_metric)
-        #self.train_metric.reset()
-        pass
     
     def validation_step(self, batch, batch_idx):
         """Functionality for prediction validation."""
@@ -89,7 +116,14 @@ class AstraModule(L.LightningModule):
         loss = self._shared_step(batch)
 
         # Log results
-        self.log('valid_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('valid/loss_step', loss, on_step=False, on_epoch=True, prog_bar=True)
+
+    def on_training_epoch_end(self):
+        # Can be used to log accumulated metrics from training_step()
+        #epoch_metric = self.train_metric.compute()
+        #self.log("train/metric_epoch", epoch_metric)
+        #self.train_metric.reset()
+        pass
 
     def on_validation_epoch_end(self, outputs):
         # Can be used to log accumulated metrics from validation_step()
@@ -103,6 +137,22 @@ trainer = L.Trainer()
 trainer.fit(model, datamodule)
 trainer.test(model, dataloaders=DataLoader(test_set)) 
 """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class XGBoostLightning(L.LightningModule):
