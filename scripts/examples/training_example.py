14dc956b382b82f5cfe760569e39c8c1a841b2d9
Maxim Kirby <eosinyx34@gmail.com>
2025-07-29 14:30:23 -0400

    Implemented seeded training (untested)

--- scripts/examples/training_example.py
+++ scripts/examples/training_example.py
@@ -6,6 +6,7 @@ from astra.constants import PROJECT_ROOT
 
 train_path = valid_path = Path.joinpath(PROJECT_ROOT, "data", "split", "cpipred", "pangenomic", "mmseqs", "train.csv")
 batch_size = 32
+seed = 42
 
 # Run training script
-train(train_path, valid_path, batch_size)
+train(train_path, valid_path, batch_size, seed)
